36587f3e3bed8e0b6552056c7ce694a69903a930
CrumpetsNTea <t.botell@gmail.com>
2021-12-14 22:12:55 -0500

    Add working test for cancelling an interview.

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -22,12 +22,27 @@ it("should book an interview", () => {
   // cy.contains(".appointment__card--show", "Sylvia Palmer");
   });
   it("should edit an interview", () => {
-    cy.get("[alt=Edit]").first().click({force:true})
-    cy.get("[alt='Tori Malcolm']").click();
+    cy.get("[alt=Edit]").first().click({force:true});
+
     cy.get("[data-testid=student-name-input]").clear().type("Steve Rogers");
+    cy.get("[alt='Tori Malcolm']").click();
+    
     cy.contains("Save").click();
 
     cy.contains(".appointment__card--show", "Steve Rogers");
     cy.contains(".appointment__card--show", "Tori Malcolm");
-  })
+  });
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").first().click({force:true});
+    
+    cy.contains("Confirm").click();
+
+    cy.contains("Deleting").should("exist");
+    cy.contains("Deleting").should("not.exist");
+
+    cy.contains(".appointment__card--show", "Archie Cohen")
+      .should("not.exist");
+
+  });
+
 });
